d456bb7dd595e666decaed5a1ec6e8c5c7e27da5
Zac022 <i.rudasingwa@alustudent.com>
2023-03-06 03:01:20 -0800

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 """
-This module contains the function minOperations(n).
+Module for minimum operations problem
 """
+
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to obtain n H's
-    in a text file given that we can only copy all and paste.
+    Method to calculate the fewest number of operations needed to result in exactly n H characters in the file.
     """
     if n <= 1:
         return 0
-    i = 2
-    operations = 0
-    while i <= n:
-        if n % i == 0:
-            operations += i
-            n //= i
-        else:
-            i += 1
-    return operations
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(n // i) + i
+        return n
 
